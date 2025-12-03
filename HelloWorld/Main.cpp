dfee7e928b0948577d902fb3c99590f7bc8a8e2a
RedKatzen <gabrieldeavilaf@gmail.com.br>
2025-12-03 10:52:59 -0300

    break: exit the iteration

--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -7,8 +7,8 @@ int main()
 	int x = 5;
 
 	for (int i = 0; i < 5; i++) {
-		if ((i + 1) % 2 == 0) // skip the iteration for every odd number
-			continue; // skip the next iteration of the loop
+		if ((i + 1) % 2 == 0)
+			break; // breaks the iteration, exiting to the next line of code
 		
 		std::cout << i;
 		Log(" Hello, World!");
