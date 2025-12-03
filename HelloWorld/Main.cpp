3d3a6a3c27c965e2ebd396ecd40e496c18fad586
RedKatzen <gabrieldeavilaf@gmail.com.br>
2025-12-03 10:59:37 -0300

    return: jumps to end of the function

--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -7,8 +7,9 @@ int main()
 	int x = 5;
 
 	for (int i = 0; i < 5; i++) {
-		if (i % 2 == 0) // skip the iteration for every pair number
-			continue; // skip the next iteration of the loop
+		if ((1 + i) % 2 == 0) 
+			return 0;	// jumps to the end of the function
+						// cautions: return in a bad position can created a "dead code"
 		
 		std::cout << i;
 		Log(" Hello, World!");
