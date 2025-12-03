cdefb76ed454a0117647d7375158bf2235c94f11
RedKatzen <gabrieldeavilaf@gmail.com.br>
2025-12-03 10:44:34 -0300

    control flow with continue

--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -7,12 +7,12 @@ int main()
 	int x = 5;
 
 	for (int i = 0; i < 5; i++) {
-		if (i > 2)
+		if (i % 2 == 0) // skip the iteration for every pair number
 			continue; // skip the next iteration of the loop
 		
 		std::cout << i;
 		Log(" Hello, World!");
 	}
-
+	 
 	std::cin.get();
 }
